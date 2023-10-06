ce105101633a0248707110ea6b1ac29e467cea37
panji2827 <panjiwidhodho@gmail.com>
2023-10-06 12:32:17 +0700

    create register.blade

---
+++ resources/views/register.blade.php
@@ -0,0 +1,19 @@
+<div class="input-group mb-3">
+    <span class="input-group-text" id="basic-addon1"></span>
+    <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+  </div>
+  
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="email" aria-label="email" aria-describedby="basic-addon2">
+    <span class="input-group-text" id="basic-addon2"></span>
+  </div>
+  
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="password" aria-label="paassword" aria-describedby="basic-addon2">
+    <span class="input-group-text" id="basic-addon2"></span>
+  </div>
+
+  <div class="input-group mb-3">
+    <input type="text" class="form-control" placeholder="password" aria-label="password" aria-describedby="basic-addon2">
+    <span class="input-group-text" id="basic-addon2"></span>
+  </div>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\NewsController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\RegiterController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,5 @@ Route::get('/', function () {
 
 Route::get ('/show',[NewsController::class,'show']);
 Route::get ('/create',[NewsController::class,'create']);
+
+Route::get ('/register',[RegiterController::class,'index']);
